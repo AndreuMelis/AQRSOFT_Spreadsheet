6bec60cc3b5779b5e7a575e7ea6c34dd3aadb583
Cristina <crisgc810@gmail.com>
2025-05-25 11:52:27 +0200

    load file

---
+++ ui/display.py
@@ -0,0 +1,23 @@
+from cell import CellRange
+from spreadsheet import Spreadsheet
+
+class DisplayContent:
+
+    def __init__(self):
+        pass
+
+    def printContentSpreadsheet(self, spreadsheet_data):
+        if not spreadsheet_data:
+            print("No data to display.")
+            return
+
+        for row in spreadsheet_data:
+            for cell_data in row:
+                print(cell_data.replace(';', '\t'), end='\t')
+            print()
+            
+    def create_new_spreadsheet(self):
+        pass
+
+    def edit_content():
+        pass
