2df2d1781f318a4ee448bbc7c5661c8c3987084e
Cristina <crisgc810@gmail.com>
2025-06-09 10:58:31 +0200

    correccions

--- usecasesmarker/spread_sheet_factory_for_checker.py
+++ usecasesmarker/spread_sheet_factory_for_checker.py
@@ -1,17 +1,12 @@
+# usecasesmarker/spread_sheet_factory_for_checker.py
+
 from usecasesmarker.spreadsheet_controller_for_checker import ISpreadsheetControllerForChecker
 
 class SpreadSheetFactoryForChecker:
-
-    ##@brief Creates and returns an object of a class that implements the methods specified within
-    # ISpreadsheetControllerForChecker (this emulates an interface for encapsulating your particular implementation of
-    # a controller, and that includes only the methods required for marking your project features). This method
-    # must create the framework of objects that allow that, once finalized its execution,
-    # everything is ready for invoking the methods of the object implementing the methods specified within
-    # ISpreadsheetControllerForChecker and starting setting contents in the spreadsheet cells and start checking results
-    # and marking your code
-    # @return an object of a class that implements the methods specified within ISpreadsheetControllerForChecker.
-
-    def create_spreadsheet_controller() -> ISpreadsheetControllerForChecker:
-        #REPLACE THE FOLLOWING CODE!!!
-        def create_spreadsheet_controller() -> ISpreadsheetControllerForChecker:
-            return ISpreadsheetControllerForChecker()
+    @staticmethod
+    def create_spreadsheet_controller():
+        """
+        Must return an object that implements all methods of
+        ISpreadsheetControllerForChecker (set_cell_content, etc.)
+        """
+        return ISpreadsheetControllerForChecker()
